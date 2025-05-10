02d959b15d5a99bc176ed89118a2f17555cffc72
kmlcsl <muhammadkamilcsl19@gmail.com>
2025-05-10 07:19:52 +0700

    Tahap routes website my portfolio

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Routes yang sudah ada
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/skills', function () {
+    return view('skills');
+})->name('skills');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/projects', function () {
+    return view('projects');
+})->name('projects');
